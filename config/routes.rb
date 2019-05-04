65c4434367c8d40bb2631dd0b52f4204c874a8b4
RhyG <rhysgeary@gmail.com>
2019-05-04 18:22:46 +1000

    Added user controller and page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,17 @@ Rails.application.routes.draw do
   resources :posts
   devise_for :users
   root to: 'posts#index'
+
+  resources :users do
+    collection do
+      get 'list'
+    end
+  end
+
+  # authenticated do
+  #   root :to => 'posts#index', as: :authenticated
+  # end
+  
+  # root :to => 'home#welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
