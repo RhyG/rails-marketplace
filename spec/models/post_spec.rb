4a206467e3fcf83fa13cd5f410f0a83355d473b4
RhyG <rhysgeary@gmail.com>
2019-05-09 12:17:00 +1000

    Added basic create and edit tests.

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  it "should create a post" do
+    post = Post.create(title:'This is a test', description: 'This is a test', category: 'Writing')
+    expect(post.title).to eq('This is a test')
+  end
+
+  it "should be able to edit a post" do
+    post = Post.create(title:'This is a test', description: 'This is a test', category: 'Writing')
+    post.update(title:'Another test')
+    expect(post.title).to eq('Another test')
+  end
+
+  it "should delete a post" do 
+    post = Post.create(id: 1, title:'This is a test', description: 'This is a test', category: 'Writing')
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    @user = User.create(id: 1, name: 'Bob', email: 'bob@email.com')
+  end
+
+  it "should create a user" do
+    user = User.create(name: 'Tester')
+    expect(user.name).to eq('Tester')
+  end
+  
+  it "should be able to edit user information" do
+    user = User.create(name: 'Tester')
+    user.update(name: 'New')
+    expect(user.name).to eq('New')
+  end
+end
